3a227505b8efecb3dea918f25c667cc07748bcdb
Rahul Pant <rahulpant2002@gmail.com>
2025-01-19 22:42:29 +0530

    Made BACKEND_URL dynamic

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "/api";
+export const BACKEND_URL = location.hostname==="localhost" ? "http://localhost:8080" : "/api";
+// export const BACKEND_URL = "http://localhost:8080";

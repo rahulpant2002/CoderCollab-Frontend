f01bc1386053ad15634eb9cef74697677660e659
Rahul Pant <rahulpant2002@gmail.com>
2025-01-24 10:10:59 +0530

    Bug Fixed - WebSocket in Production

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,6 +4,6 @@ import { BACKEND_URL } from "./constant";
 export const createSocketConnection = () => { 
     if(location.hostname==="localhost") return new io(BACKEND_URL);
     else{
-        return new io('/api', {path : '/api/socket.io'});
+        return new io('/', {path : '/api/socket.io'});
     }
 };
